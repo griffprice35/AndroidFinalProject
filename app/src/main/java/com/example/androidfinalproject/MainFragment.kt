63b520d78c49968500a1e9cb9fccb43579fedf61
griffprice35 <griffin.price@comcast.net>
2021-05-06 13:21:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/androidfinalproject/MainFragment.kt
@@ -0,0 +1,14 @@
+package com.example.androidfinalproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+        return rootView
+    }
+}
