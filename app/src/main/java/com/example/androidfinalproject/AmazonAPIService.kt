1e1c3f511951b869561935588c8b9fbc60b11ee3
griffprice35 <griffin.price@comcast.net>
2021-06-04 01:12:03 -0400

    same thing with clash api instead

--- app/src/main/java/com/example/androidfinalproject/AmazonAPIService.kt
+++ app/src/main/java/com/example/androidfinalproject/AmazonAPIService.kt
@@ -9,9 +9,13 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val QUERY_STRING =
-    "?url=https://www.amazon.com/dp/B07SXMZLPK/ref=cm_sw_r_tw_dp_94DQDM4JDZDCS6BNRF48"
+    "players/#2LUORYYG"
 
-private const val BASE_URL = "http://api-prd.axesso.de/amz/amazon-lookup-product/"
+private const val BASE_URL = "https://api.clashroyale.com/v1/"
+
+private const val API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImRhNjljMjI2LWEyODQtNDQxZi1iMGUwLWZiYWUwYzlhZTNmYiIsImlhdCI6MTYyMjc3Njc1NSwic3ViIjoiZGV2ZWxvcGVyL2M2M2Q0NmE5LWJmYzgtYjg0Ny1jZGRkLWExNzVlNDE2YzM4MSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDYuMjE5LjY3LjEzMCIsIjczLjEwMS4xOTMuMjA1Il0sInR5cGUiOiJjbGllbnQifV19.IlWdDdpHunprLvJx5vXdRxBcIQ_dcWoUsLGZ6i9z8037vxt5wKS8Ez8kPgBkea5pBYO5s5-DJCyeN41ffS7AKg"
+
+
 
 /*
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
